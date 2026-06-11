a993a0af5c448cef1ad2cfd282b479624dd6e7ba
solowrldwide-oss <solowrldwide@gmail.com>
2026-06-11 02:48:53 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,62 +1,11 @@
-const API = "http://localhost:3000";
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-let token = "";
-
-async function login(){
-  const res = await fetch(API+"/login",{
-    method:"POST",
-    headers:{ "Content-Type":"application/json" },
-    body: JSON.stringify({
-      email: email.value,
-      password: password.value
-    })
-  });
-
-  const data = await res.json();
-
-  token = data.token;
-  localStorage.setItem("token", token);
-
-  location.href="dashboard.html";
-}
-
-async function load(){
-  token = localStorage.getItem("token");
-
-  const res = await fetch(API+"/me",{
-    headers:{ Authorization: token }
-  });
-
-  const user = await res.json();
-
-  document.getElementById("balance").innerText =
-    "$" + user.balance;
-}
-
-async function send(){
-  const res = await fetch(API+"/transfer",{
-    method:"POST",
-    headers:{ "Content-Type":"application/json" },
-    body: JSON.stringify({
-      fromId: 1,
-      walletId: walletId.value,
-      amount: Number(amount.value)
-    })
-  });
-
-  const data = await res.json();
-  alert("Transfer complete");
-}
-
-async function loadHistory(){
-  const res = await fetch(API+"/transactions");
-  const data = await res.json();
-
-  list.innerHTML = data.map(t =>
-    `<div class="card">
-      ${t.from} → ${t.to}<br>
-      $${t.amount}<br>
-      ${new Date(t.date).toDateString()}
-    </div>`
-  ).join("");
+  if (email === "theethanerivers@gmail.com" && password === "Incredibleman") {
+    sessionStorage.setItem("loggedIn", "true");
+    window.location.href = "dashboard.html";
+  } else {
+    document.getElementById("err").innerText = "Invalid login details";
+  }
 }
